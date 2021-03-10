faca76486a803995be7f345e2bbe677f987f9e24
Guilherme Policarpo <74926145+guipolicarpo@users.noreply.github.com>
2021-03-09 22:28:27 -0300

    Add files via upload

---
+++ exc2.py
@@ -0,0 +1,5 @@
+dia = input('Qual o dia que você nasceu?: ')
+mes = input('Qual o mês que você nasceu?: ')
+ano = input('Qual o ano que você nasceu?: ')
+
+print('Sua data de nascimento é ' + dia + '/' + mes + '/' + ano)
