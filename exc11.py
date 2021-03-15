63213019a657f49e01b1b142309398d9c39e940b
Guilherme Policarpo <74926145+guipolicarpo@users.noreply.github.com>
2021-03-15 10:07:12 -0300

    Add files via upload

---
+++ exc11.py
@@ -0,0 +1,4 @@
+altura = float(input('Qual a altura da parede: '))
+largura = float(input('Qual a largura da parede: '))
+print('A área do parede é {}m2 e a quantidade de litros necessários de tinta é {}'.format(altura*largura, (altura*largura)/2))
+
---
+++ exc13.py
@@ -0,0 +1,4 @@
+salario = float(input('Qual o seu salário: '))
+salario += salario*0.15
+print('Seu novo salário com 15% de aumento é R${:.2f}'.format(salario))
+
