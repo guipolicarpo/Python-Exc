a1c2e80be74eea6037888752d5c73841c52000ca
Guilherme Policarpo <74926145+guipolicarpo@users.noreply.github.com>
2021-04-01 21:09:23 -0300

    Add files via upload

---
+++ exc27.py
@@ -0,0 +1,5 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+nome = nome.split()
+print('O seu primeiro nome é {}'.format(nome[0]))
+print('O seu ultimo nome é {}'.format(nome[len(nome)-1]))
+
